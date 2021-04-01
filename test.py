bbdb287bcc0b3ddef443df4fb806adc782bce740
AgentMrBig <agentmrbig@gmail.com>
2021-04-01 00:09:30 -0400

    testing out crud to dictionary, next will be crud to sql database

--- test.py
+++ test.py
@@ -2,9 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(
-    BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100000})
-print(response.json())
+data = [{"likes": 10, "name": "Joe", "views": 100000},
+        {"likes": 10, "name": "How to make Rest Api", "views": 80000},
+        {"likes": 10, "name": "Tim", "views": 4500}, ]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
 input()
-response = requests.get(BASE + "video/1")
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/6")
 print(response.json())
